344eafa908cff1da61cd414d78bd55dcba6ee84d
franck-benault <franck-benault@orange.fr>
2015-06-05 23:03:04 +0200

    first example with isEqualTo

--- src/test/java/net/franckbenault/testassertj/assertion/s01basicjunit/JUnitTestCase.java
+++ src/test/java/net/franckbenault/testassertj/assertion/s01basicjunit/JUnitTestCase.java
@@ -1,14 +1,18 @@
 package net.franckbenault.testassertj.assertion.s01basicjunit;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Test;
 
 public class JUnitTestCase {
 
 	@Test
-	public void test() {
-		assertTrue(true);
+	public void t02aAssertEquals() {
+		String s1 = "String";
+		
+		assertThat(s1).isEqualTo("String");
+		assertThat(s1).as("s1").isEqualTo("String");
+		
 	}
 
 }
