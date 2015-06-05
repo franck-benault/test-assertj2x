86512116e25c4e7f0d1573eaefdd90c07fa2a31a
franck-benault <franck-benault@orange.fr>
2015-06-05 22:50:05 +0200

    init project

---
+++ src/test/java/net/franckbenault/testassertj/assertion/s01basicjunit/JUnitTestCase.java
@@ -0,0 +1,14 @@
+package net.franckbenault.testassertj.assertion.s01basicjunit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JUnitTestCase {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
